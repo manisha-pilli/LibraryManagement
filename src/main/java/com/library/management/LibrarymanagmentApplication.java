6b0ff9224f35c4da969cb09ff62fc750d0001866
manisha <pillimanisha2@gmail.com>
2024-02-28 15:57:01 +0530

    Initial commit

---
+++ src/main/java/com/library/management/LibrarymanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.library.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagmentApplication.class, args);
+	}
+
+}
